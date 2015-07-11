9fda23c56816617256bad86bcaf5a0c352c7a468
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-11 06:27:17 -0400

    better dotfile handling

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 'use strict';
 
 var lazy = require('lazy-cache')(require);
-var extend = lazy('extend-shallow');
+var isDotfile = require('is-dotfile');
+var isDotdir = require('is-dotdir');
 var lookup = lazy('look-up');
 var ignore = lazy('parse-gitignore');
 var mm = lazy('micromatch');
 var cwd = process.cwd();
 
-function parseGitignore(opts) {
+function parseGitignore(opts, app) {
   opts = opts || {};
-  var filepath = lookup()('.gitignore', {cwd: cwd});
-  var patterns = ignore()(filepath);
 
-  var isMatch = function (filepath) {
-    return mm().any(patterns, filepath, opts);
+  if (typeof app !== 'undefined') {
+    opts = app.setDefaults(app.pattern.options, opts);
+  }
+
+  var gitignoreFile = lookup()('.gitignore', {cwd: cwd});
+  var ignorePatterns = ignore()(gitignoreFile);
+
+  var isMatch = function (fp) {
+    return mm().any(ignorePatterns, fp, opts);
   };
 
   return function gitignore(file) {
-    opts = extend()({}, this.options, this.pattern.options, opts);
+    this.handler.emit('gitignore', file);
+
+    if (typeof this === 'undefined') {
+      opts = this.setDefaults(this.pattern.options, opts);
+    }
+
+    if (opts.dot || opts.dotfiles || opts.dotdirs) {
+      if (isDotfile(file.relative) || isDotdir(file.relative)) {
+        return file;
+      }
+    }
 
     if (opts.gitignore === false) {
       return file;
     }
 
-    if (isMatch(file.path)) {
+    if (isMatch(file.relative)) {
+      file.isIgnored = true;
       file.exclude = true;
     }
     return file;
