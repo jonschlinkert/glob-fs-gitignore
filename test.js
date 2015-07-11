8be5877ff6eae6d6cfd33de1e32486fe621438fc
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-11 06:27:41 -0400

    0.1.4

--- test.js
+++ test.js
@@ -12,20 +12,35 @@ function has(files, fp) {
 }
 
 describe('gitignore', function (done) {
+  var tempfiles = ['.foo', '.bar', '.baz'];
+  before(function () {
+    tempfiles.forEach(function (fp) {
+      fs.writeFileSync(fp, 'temp');
+    });
+  });
+
+  after(function () {
+    tempfiles.forEach(function (fp) {
+      fs.unlinkSync(fp);
+    });
+  });
+
   it('should ignore files specified in `.gitignore` automatically:', function (done) {
     glob()
       .use(gitignore())
-      .readdir('**/*', function (err, files) {
+      .readdir('*', function (err, files) {
         assert.equal(has(files, '.DS_Store'), false);
         done();
       });
   });
 
   it('should not use `.gitignore` when `gitignore: false` is passed:', function (done) {
-    glob({ gitignore: false, builtins: false })
+    glob({ gitignore: false, dot: true })
       .use(gitignore())
-      .readdir('**/*', function (err, files) {
-        assert.equal(has(files, '.DS_Store'), true);
+      .readdir('*', function (err, files) {
+        assert.equal(has(files, '.foo'), true);
+        assert.equal(has(files, '.bar'), true);
+        assert.equal(has(files, '.baz'), true);
         done();
       });
   });
